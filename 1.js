d29daeff4ebfd36a1efa8cf351f825fc892f0da2
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2021-03-07 14:05:30 +0530

    length of the every element in the array

---
+++ 1.js
@@ -0,0 +1,21 @@
+const countries = [
+	'ALBANIA',
+	'BOLIVIA',
+	'CANADA',
+	'DENMARK',
+	'ETHIOPIA',
+	'FINLAND',
+	'GERMANY',
+	'HUNGARY',
+	'IRELAND',
+	'JAPAN',
+	'KENYA',
+];
+
+// find array elements length
+var length = countries.map((country) => { return country.length });
+
+console.log(length);
+
+// create an array with first three letters of every element in an array countries
+
